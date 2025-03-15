a3edcb6a1721412a16c9050a5aa1ff578be8abf8
nMWGecko <joshvilda@gmail.com>
2025-03-15 17:03:59 -0400

    Adjusted to work on other PCs

--- READMEINSTALLATION.md
+++ READMEINSTALLATION.md
@@ -1,7 +1,6 @@
-Dependacies:
-
-csv
-tensorflow
-keras
-numpy
+Dependencies:
+Python 3.12.9
+tensorflow  2.19.0
+keras  3.9.0
+numpy  1.26.3
 
--- run.py
+++ run.py
@@ -16,28 +16,27 @@ model = tf.keras.models.load_model("brain_mri_classifier_epoch_11.keras")
 class_names = ["no", "yes"]
 
 # Loop through subdirectories in the dataset directory
-for subdir in os.listdir(dataset_dir):
-    subdir_path = path.join(dataset_dir, subdir)
+subdir_path = os.path.join(dataset_dir, 'CEC_test')
 
-    if os.path.isdir(subdir_path):
-        subdir_path_list = os.listdir(subdir_path)
-        for brain in subdir_path_list:
-            brainPath = path.join(dataset_dir,subdir,brain)
-            print(brainPath)
-            img = image.load_img(brainPath, target_size=(224, 224)) 
-            img_array = image.img_to_array(img)
-            img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
-            img_array = img_array / 255.0  # Normalize (same preprocessing as training)
-            # Make prediction
-            predictions = model.predict(img_array)
+#Modified from ChatGPT
+if os.path.isdir(subdir_path):
+    subdir_path_list = os.listdir(subdir_path)
+    for brain in subdir_path_list:
+        brainPath = path.join(subdir_path,brain)
+        print(brainPath)
+        img = image.load_img(brainPath, target_size=(224, 224)) 
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+        img_array = img_array / 255.0  # Normalize (same preprocessing as training)
+        # Make prediction
+        predictions = model.predict(img_array)
 
-            # Get the class with the highest probability
-            predicted_class = class_names[np.argmax(predictions)]
-            confidence = np.max(predictions)          
-            csv_array_appender(brain,predicted_class,confidence,outputList)
-            print(f"Predicted class: {predicted_class} with {confidence:.2f} confidence")  
+        # Get the class with the highest probability
+        predicted_class = class_names[np.argmax(predictions)]
+        confidence = np.max(predictions)          
+        csv_array_appender(brain,predicted_class,confidence,outputList)
+        print(f"Predicted class: {predicted_class} with {confidence:.2f} confidence")  
 
 #Export to CSV
 csv_output(outputList)
-
 
